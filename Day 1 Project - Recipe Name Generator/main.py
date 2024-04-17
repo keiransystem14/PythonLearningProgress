1a64551e770047bebca0c523c4f2a9079ded8da8
keiransystems14 <keiran1412@hotmail.com>
2024-04-17 19:34:42 +0100

    add new line in the program

--- Day 1 Project - Recipe Name Generator/main.py
+++ Day 1 Project - Recipe Name Generator/main.py
@@ -1,14 +1,14 @@
 # I answer my name. The answer replaces the input function and stores the name into a new variable called name.
 Name = input("What is your name? \n")
-
+print("\n")
 # What is your favourite country
 # I then answer my favourite country which replaces the input function and it then stores the answer into a new variable called Country
 Country = input ("What is your favourite country? \n")
-
+print("\n")
 # What is your favourite food
 # I answer what's my favourite food which does replace the input function, the answer replaces the input function and it then stores it into a new variable called food
 Food = input("What is your favourite food? \n")
-
+print("\n")
 #Use string concatenation to combine all the answers into one.
 print(Name + " " + Country + " " + Food)
 
