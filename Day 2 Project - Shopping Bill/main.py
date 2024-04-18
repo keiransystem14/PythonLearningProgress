a39536025342b4fde6d022ef699ffb1ab1cc6240
keiransystems14 <keiran1412@hotmail.com>
2024-04-18 23:34:19 +0100

    Calculate the total price before VAT and practice the print using f string

--- Day 2 Project - Shopping Bill/main.py
+++ Day 2 Project - Shopping Bill/main.py
@@ -1,3 +1,13 @@
-#Asking the customer for the price of item 1
+# Asking the customer for the price of item 1. Onced entered, the value replaced the input function.
 
 item_1 = input("What is the price of item 1 \n")
+item_2 = input("What is the price of item 2 \n")
+
+# Calculate the total price of both items
+
+total_before_vat = float(item_1) + float(item_2)
+
+print(f"The total price before VAT is {total_before_vat}.")
+
+# Example would be 5% Tax
+#VAT = float(0.5)
