45f2cd694222e29c52797b23a08ed9ecb10d6012
keiransystems14 <keiran1412@hotmail.com>
2024-04-27 20:31:54 +0100

    Created a way to randomly generate letters and print out the output of those letters

--- Day 5 Project - Security Password Generator/main.py
+++ Day 5 Project - Security Password Generator/main.py
@@ -1,3 +1,5 @@
+import random
+
 letters = [
     "a",
     "b",
@@ -57,6 +59,29 @@ symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 
 print("*** Welcome to Security Password Generator ***")
 
+# Asking the user to input how many letters, numbers and symbols to generate in a password.
+
 number_of_letters = int(input("How many letters would you like in your password? \n"))
 number_of_symbols = int(input("How many symbols would you like in your password? \n"))
 number_of_numbers = int(input("How many numbers would you like in your password? \n"))
+
+# Empty password
+password = []
+
+# For loop for letters is used to randomly generate numbers from 1 to the number of letters the user wants.
+# Then use the random.choice() function to randomly generate the number of letters (strings) from the letters list.
+# Once the number of letters is randomly generated, it passes the letters from variable generate_letter variale into an empty into a password variable which contains an empty list.
+
+for generate_letter in range(1, number_of_letters + 1):
+    generate_letter = random.choice(letters)
+    password += generate_letter
+
+# Then concactinate the strings within the list using .join() function and print the randomly generated password in letters.
+
+generate_password = "".join(password)
+print(generate_password)
+
+
+# For loop for numbers
+
+# For loop for symbols
