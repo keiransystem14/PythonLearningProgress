057b609dd045a4296190490ed9cfc79da56ca88c
keiransystems14 <keiran1412@hotmail.com>
2024-04-28 11:52:40 +0100

    Improve the output of the program by adding \n in there

--- Day 5 Project - Security Password Generator/main.py
+++ Day 5 Project - Security Password Generator/main.py
@@ -57,13 +57,13 @@ letters = [
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 
-print("*** Welcome to Security Password Generator ***")
+print("\n*** Welcome to Security Password Generator ***")
 
 # Asking the user to input how many letters, numbers and symbols to generate in a password.
 
-number_of_letters = int(input("How many letters would you like in your password? \n"))
-number_of_symbols = int(input("How many symbols would you like in your password? \n"))
-number_of_numbers = int(input("How many numbers would you like in your password? \n"))
+number_of_letters = int(input("\nHow many letters would you like in your password? \n"))
+number_of_symbols = int(input("\nHow many symbols would you like in your password? \n"))
+number_of_numbers = int(input("\nHow many numbers would you like in your password? \n"))
 
 # Empty password
 password = []
@@ -71,7 +71,7 @@ password = []
 # For loop for letters is used to randomly generate numbers from 1 to the number of letters the user wants.
 # Then use the random.choice() function to randomly generate the number of letters (strings) from the letters list.
 # Once the number of letters is randomly generated, it passes the letters from variable generate_letter variale into an empty into a password variable which contains an empty list.
-# Random.shuffle() function is where it re-organizes the letters, numbers and symbols in the list.
+# Random.shuffle() function is where it re-organizes the letters, numbers and symbols in the list to make the password more complex.
 # Then concactinate the strings within the list using .join() function and print the randomly generated password in letters.
 
 for generate_letter in range(1, number_of_letters + 1):
@@ -93,6 +93,4 @@ for generate_numbers in range(1, number_of_numbers + 1):
 # Then concactinate the strings within the list using .join() function after letters, numbers and symbols are randomly generated in a list
 
 join_password = "".join(password)
-print(join_password)
-
-# Use Random.shuffle function to swap the characters to make the password complex.
+print(f"\nGenerated Password: {join_password}\n")
