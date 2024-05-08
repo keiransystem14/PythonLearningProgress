c6778ac7228dc954142c229948855ad4ebe8e617
keiransystems14 <keiran1412@hotmail.com>
2024-05-08 17:43:48 +0100

    Replacing blanks with the user guess

--- Day 7 Project - Hangman/main.py
+++ Day 7 Project - Hangman/main.py
@@ -2,27 +2,37 @@ import random
 
 # Create a Word list using Python lists
 
-word_list = ["brazil", "germany", "peru"]
+word_list = ["canada", "ghana", "germany"]
+
+# Empty list called display
+display = []
 
 # Choose a word randomly using random.choice function within the random module to choose a word from the list randomly
 
 random_word = random.choice(word_list)
-random_word.split()
+
+# Pass _ into display list python in a iteration until the last word is checked.
+
+for letter in random_word:
+    display += "_"
 
 
 # Ask the user to guess a letter
-
 user_guess = input("Guess a letter: ")
 
-# Blank words
-blank_list = []
+# Initialized at position 0
+position = 0
 
-# It scans through each letter in the word to see if it matches with the user guess. If it does, it would pass the user guess into a blank word python list and print out correct letter. If user guess is incorrect, it will print out incorrect letter.
-# This will be done in a iteration until all the letters in the word have been checked.
-for check_word in random_word:
-    if check_word == user_guess:
-        blank_list.append(user_guess)
-        print("Correct letter")
-
+# Iterates each letter in the random word
+for check_letter in random_word:
+    # If user's guess matches with current letter in the random word
+    if check_letter in user_guess:
+        # Replace the "_" at the current position with the user guess
+        display[position] = user_guess
     else:
         print("Incorrect letter")
+
+    # Move to the next position on the display list
+    position += 1
+
+print(display)
