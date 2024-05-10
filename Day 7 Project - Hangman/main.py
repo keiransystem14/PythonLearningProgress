63c6fa1a3ec52839f85a38f5c98bd47d70e17f28
keiransystems14 <keiran1412@hotmail.com>
2024-05-10 04:47:13 +0100

    The hangman game is nearly completed. I have just added a functionality where it keeps track of players lives.

--- Day 7 Project - Hangman/main.py
+++ Day 7 Project - Hangman/main.py
@@ -1,5 +1,70 @@
 import random
 
+stages = [
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+]
+
 # Create a Word list using Python lists
 
 word_list = ["canada", "ghana", "germany"]
@@ -11,22 +76,50 @@ display = []
 
 random_word = random.choice(word_list)
 
+# The user starts with 6 lives.
+lives = 6
+
 # Pass _ into display list python in a iteration until the last word is checked.
 
 for letter in random_word:
     display += "_"
 
+# We assume that the hangman game is running
+hangman_game_running = True
 
-# Ask the user to guess a letter
-user_guess = input("Guess a letter: ")
+while hangman_game_running:
 
-# Iterates each letter in the random word using range function to loop through between 0 and the end of the random word.
-for position in range(len(random_word)):
-    # It will assign the letter from random_word at position 0 to a variable called letter.
-    letter = random_word[position]
-    # If user's guess matches with the current letter within the position of the random word
-    if letter == user_guess:
-        # Replace the "_" at the current position with the user guess
-        display[position] = letter
+    # It loops through the condition until there isn't any blanks left in the display list
+    if "_" in display:
 
-print(display)
+        # Ask the user to guess a letter
+        user_guess = input("Guess a letter: ")
+
+        if user_guess in random_word:
+            # Iterates each letter in the random word using range function to loop through between 0 and the end of the random word.
+            for position in range(len(random_word)):
+                # It will assign the letter from random_word at position 0 to a variable called letter.
+                letter = random_word[position]
+                # If user's guess matches with the current letter within the position of the random word
+                if letter == user_guess:
+                    # Replace the "_" at the current position with the user guess
+                    display[position] = letter
+                    print(display)
+
+        # At each turn if the guess is wrong, they lose a life.
+        else:
+            lives -= 1
+            # Update the current stage based on the number of incorrect guesses
+            current_index = min(lives, len(stages) - 1)
+            # Print current stage
+            print(stages[current_index])
+
+        if lives == 0:
+            print("You Lose!!")
+            hangman_game_running = False
+
+    else:
+        # Join the elements in the python list into one word
+        join_string = "".join(display)
+        print(join_string)
+        print("You win the game!!")
