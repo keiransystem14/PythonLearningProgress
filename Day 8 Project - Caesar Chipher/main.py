4c57faf125d58540dc8ed455450fb2e6b3fca2c9
keiransystems14 <keiran1412@hotmail.com>
2024-05-17 17:58:36 +0100

    improved the code by combining both encrypt and decrypt function into one function

--- Day 8 Project - Caesar Chipher/main.py
+++ Day 8 Project - Caesar Chipher/main.py
@@ -32,77 +32,71 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(plain_text, forward_shift):
+def caesar(direction, text, shift):
 
-    # Empty python list
-    encrypted_string = []
+    encoded_string = []
+    plaintext_string = []
 
-    # It iterates through the text the user has inputted
-    for position in range(0, len(plain_text)):
+    # Checks if the user wanted to encrypt or decrypt by checking the directon variable with IF statements.
+    if direction == "encode":
 
-        # Starts going through the text by each character.
-        letter_check = plain_text[position]
+        for position in range(0, len(text)):
 
-        # It iterates through the alphabet in order.
-        for alpha_check in alphabet:
+            # Starts going through the text by each character.
+            letter_check = text[position]
 
-            # It checks if the character from the text is the same as the character in the alphabet.
-            if alpha_check == letter_check:
+            # It iterates through the alphabet in order.
+            for alpha_check in alphabet:
 
-                # It the position of the letter inside the alphabet python list using the index() function.
-                index = alphabet.index(alpha_check)
+                # It checks if the character from the text is the same as the character in the alphabet.
+                if alpha_check == letter_check:
 
-                # Shifts each letter in the text forwards as part of encrypting the text. Also to ensure it stays within the range of alphabets.
-                new_position = (index + forward_shift) % len(alphabet)
+                    # It the position of the letter inside the alphabet python list using the index() function.
+                    index = alphabet.index(alpha_check)
 
-                # Adds the a new letter within the index of the new position into empty python list
-                encrypted_string += alphabet[new_position]
+                    # Shifts each letter in the text forwards as part of encrypting the text. Also to ensure it stays within the range of alphabets.
+                    new_position = (index + shift) % len(alphabet)
 
-    # Combines all the letters into one word to complete the encrypted message.
-    encrypted_message = "".join(encrypted_string)
+                    # Adds the a new letter within the index of the new position into empty python list
+                    encoded_string += alphabet[new_position]
 
-    # prints the enrcrypted message
-    print(f"The encoded text is: {encrypted_message}")
+        # Combines all the letters into one word to complete the encrypted message.
+        encrypted_message = "".join(encoded_string)
+
+        # prints the enrcrypted message
+        print(f"The {direction} text is: {encrypted_message}")
+
+    elif direction == "decode":
+
+        # It iterates through the text the user has inputted
+        for position in range(0, len(text)):
+
+            # Starts going through the text by each character.
+            letter_check = text[position]
+
+            # It iterates through the alphabet in order.
+            for alpha_check in alphabet:
+
+                # It checks if the character from the text is the same as the character in the alphabet.
+                if alpha_check == letter_check:
+
+                    # It the position of the letter inside the alphabet python list using the index() function.
+                    index = alphabet.index(alpha_check)
+
+                    # Shifts each letter in the text backwards as part of decrypting it into plain text. Also to ensure it stays within the range of alphabets.
+                    new_position = (index - shift) % len(alphabet)
+
+                    # Adds the a new letter within the index of the new position into empty python list
+                    plaintext_string += alphabet[new_position]
+
+        # Combines all the letters into one word to complete the decrypting encoded text into plain text.
+        decrypt_message = "".join(plaintext_string)
+
+        # prints the enrcrypted message
+        print(f"The {direction} text is: {decrypt_message}")
+    else:
+        print("This option doesn't exist.")
 
 
-def decrypt(encoded_text, backward_shift):
-
-    # Empty python list
-    decrypt_string = []
-
-    # It iterates through the text the user has inputted
-    for position in range(0, len(encoded_text)):
-
-        # Starts going through the text by each character.
-        letter_check = encoded_text[position]
-
-        # It iterates through the alphabet in order.
-        for alpha_check in alphabet:
-
-            # It checks if the character from the text is the same as the character in the alphabet.
-            if alpha_check == letter_check:
-
-                # It the position of the letter inside the alphabet python list using the index() function.
-                index = alphabet.index(alpha_check)
-
-                # Shifts each letter in the text backwards as part of decrypting it into plain text. Also to ensure it stays within the range of alphabets.
-                new_position = (index - backward_shift) % len(alphabet)
-
-                # Adds the a new letter within the index of the new position into empty python list
-                decrypt_string += alphabet[new_position]
-
-    # Combines all the letters into one word to complete the decrypting encoded text into plain text.
-    decrypt_message = "".join(decrypt_string)
-
-    # prints the enrcrypted message
-    print(f"The decoded text is: {decrypt_message}")
-
-
-# Checks if the user wanted to encrypt or decrypt by checking the directon variable with IF statements.
-
-if direction == "encode":
-    encrypt(plain_text=text, forward_shift=shift)
-elif direction == "decode":
-    decrypt(encoded_text=text, backward_shift=shift)
-else:
-    print("This option doesn't exist.")
+# Call caesar function with parameters to execute the function.
+caesar(direction, text, shift)
