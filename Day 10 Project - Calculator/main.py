98bb111bf049d5664645d61a7a7906142a25db66
keiransystems14 <keiran1412@hotmail.com>
2024-06-01 10:06:56 +0100

    Adding the Calculator logo and writing the input function to gather user response to the questions

---
+++ Day 10 Project - Calculator/main.py
@@ -0,0 +1,18 @@
+from art import *
+
+# Print the calulator logo
+print(logo)
+
+# User picks the first number
+first_number = input("What is the first number?: ")
+
+# Prompt the user to pick a mathmatical operation
+math_operator = input("Pick an math Operation:")
+
+# User picls the second number
+second_number = input("What is the second number?: ")
+
+# Prompt user if they want to start a new calculation
+continue_calc = input(
+    "Type 'y' to continue calculating with total, or type 'n' to start a new calculatio"
+)
