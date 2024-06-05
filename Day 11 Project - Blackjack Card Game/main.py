9ea5eddef86347d59cef7b676134c5275839c6c6
keiransystems14 <keiran1412@hotmail.com>
2024-06-05 18:12:01 +0100

    Add the totals of the two cards that user and computer drew in random

--- Day 11 Project - Blackjack Card Game/main.py
+++ Day 11 Project - Blackjack Card Game/main.py
@@ -16,12 +16,28 @@ def deal_cards():
     # Iterates the loop two times until both user and computer recieves two cards
     for _ in range(hand_size):
 
-        # It chooses generates a card in random and passes it over to user and computer list. 
+        # It chooses generates a card in random and passes it over to user and computer list.
         user.append(random.choice(cards))
         computer.append(random.choice(cards))
 
+
 def calculate_score():
 
-    
+    user_first_hand = user[0]
+    user_second_hand = user[1]
+
+    computer_first_hand = computer[0]
+    computer_second_hand = computer[1]
+
+    # Total score for user
+    user_total_score = user_first_hand + user_second_hand
+
+    # Total score for computer
+    computer_total_score = computer_first_hand + computer_second_hand
+
+    print(user_total_score)
+    print(computer_total_score)
+
 
 deal_cards()
+calculate_score()
