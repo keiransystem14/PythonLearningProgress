ff05b8e1c2fa68c8809d459e0c27305b652b0f1f
keiransystems14 <keiran1412@hotmail.com>
2024-10-09 19:07:50 +0100

    Create a deal_card() function and try to deal a single card from a integer list.

--- Day 11 Project - Blackjack Card Game/main.py
+++ Day 11 Project - Blackjack Card Game/main.py
@@ -1,3 +1,25 @@
 import random
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+# Create a deal_card() function that uses the List below to return a random card.
+
+def deal_card():
+
+    #List of cards in the deck.
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    # Choose a card randomly from the cards list
+    card = random.choice(cards)
+    return card
+
+user_cards = []
+computer_cards = []
+
+user_cards.append(deal_card())
+computer_cards.append(deal_card())
+
+print(user_cards)
+print(computer_cards)
+#def compare():
+
+#def calculate_score():
+
