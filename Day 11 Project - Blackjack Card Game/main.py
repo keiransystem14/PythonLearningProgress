df3e4ff5add3b3f6ad7677cb1876ff88917e1574
keiransystems14 <keiran1412@hotmail.com>
2024-10-13 20:16:11 +0100

    Wrote code for the compare function which compares the user's total score to the computer's total score.

--- Day 11 Project - Blackjack Card Game/main.py
+++ Day 11 Project - Blackjack Card Game/main.py
@@ -26,7 +26,25 @@ def calculate_score(cards):
     # It returns the total score of the cards.
     return sum(cards)
 
-#def compare():
+# Compare function is created to demonstrate a comparison between user's total score and computer's total score.
+def compare(user_score, computer_score):
+
+    if user_score == computer_score:
+        print("It's a Draw!!")
+    elif computer_score == 0:
+        print("Computer has a blackjack. You Lose!!")
+    elif user_score == 0:
+        print("You have a Blackjack! You Lose!")
+    elif computer_score > 21:
+        print("Computer's score went above 21. You win!!")
+    elif user_score > 21:
+        print("Your score went above 21. You Lose!!")
+    elif user_score > computer_score:
+        print("You win")
+    else:
+        print("You Lose!!")
+
+
 
 
 
@@ -65,9 +83,18 @@ def play_game():
         user_score = calculate_score(user_cards)
         computer_score = calculate_score(computer_cards)
 
-
+        # Outputs the cards user has and the total score from cards they have on their hand.
         print(f"Your cards: {user_cards}, current score: {user_score}.")
         print("Computer's First card is:", computer_cards[0])
+
+
+        """
+        If the user score is above 21, then game is over and the condition changes to false. However, it the 
+        score is below 21 then it asks the user if they want to continue dealing. 
+        
+        If the user wants to continue deal, they can draw another card from the deck and adds it to the 
+        score for the user. If the user doesn't want to deal then the condition changes game over to be true. 
+        """
 
         if user_score == 0 or computer_score == 0 or user_score > 21:
             game_over = True
@@ -79,14 +106,19 @@ def play_game():
             elif user_should_deal == "no":
                 game_over = True
 
+    """
+    If the computer's total score is below 17, then it will keep drawing the cards until the total score goes above 17. 
+    """
+
     while computer_score != 0 and computer_score < 17:
         computer_cards.append(deal_card())
         computer_score = calculate_score(computer_cards)
 
     print(f"Your final hand: {user_cards}, final score: {user_score}.")
     print(f"Computer's hand: {computer_cards}, final score: {computer_score}.")
+    print(compare(user_score, computer_score))
 
-
+# The program asks the user if they want to play Blackjack.
 play = input("Do you want to play the game of Blackjack 'Yes' or 'No': ")
 
 if play == "Yes".lower():
